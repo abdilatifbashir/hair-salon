4ce53ca26cbc4a6af8c0bc3d22b64e12a18644ad
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-03 15:01:07 +0300

    add constructor and getters methods

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,3 +2,37 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
+
+public class Task {
+  private String description;
+  private boolean completed;
+  private LocalDateTime createdAt;
+  private int id;
+  private int categoryId;
+
+  public Task(String description, int categoryId) {
+    this.description = description;
+    completed = false;
+    createdAt = LocalDateTime.now();
+    this.categoryId = categoryId;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public int getCategoryId() {
+    return categoryId;
+  }
+
+  public int getId() {
+    return id;
+  }
