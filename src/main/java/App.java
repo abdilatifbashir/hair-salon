7d32516e8d0186f02fc882e6df298540ad8980d2
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-03 18:27:25 +0300

    add stylist root that renders stylists.vtl to display list of stylists

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,8 +24,26 @@ public class App {
         get("/", (request, response) -> {
           Map<String, Object> model = new HashMap<String, Object>();
           model.put("template", "templates/index.vtl");
-          model.put("categories", Stylist.all());
+          model.put("stylists", Stylist.all());
           return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+
+    post("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Stylist newStylist = new Stylist(name);
+      newStylist.save();
+      model.put("template", "templates/stylist-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  model.put("stylists", Stylist.all());
+  model.put("template", "templates/stylists.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+
     }
   }
