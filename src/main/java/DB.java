8e2b4e904ce44db7bf88f01c2479f4cd2e7fbab6
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-04 13:13:52 +0300

    add css style

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,18 +1,22 @@
 import org.sql2o.*;
 /*import java.net.URI;
-import java.net.URISyntaxException;*/
-
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "abdilatif", "6823");
-}
+public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "abdilatif", "6823");
 
+/*  private static URI dbUri;
+    public static Sql2o sql2o;
 
-    /*static {
+    Logger logger = LoggerFactory.getLogger(DB.class);
+
+    static {
 
         try {
             if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/hair-salon");
+                dbUri = new URI("postgres://localhost:5432/hair_salon");
             } else {
                 dbUri = new URI(System.getenv("DATABASE_URL"));
             }
@@ -25,5 +29,8 @@ public class DB {
 
             sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
         } catch (URISyntaxException e ) {
+
         }
-    }*/
+    }
+    */
+}
