00672af197d2df03d85e48b9934e7db3c770607f
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-04 13:45:00 +0300

    add root and velocity template that adds client

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -51,5 +51,16 @@ get("/stylists/new", (request, response) -> {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+post("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+      String name = request.queryParams("name");
+      Client newClient = new Client(name, stylist.getId());
+      newClient.save();
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist-client-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     }
   }
